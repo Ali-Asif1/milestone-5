af37e1a35f26e8db62c1d4c7340bf6fee536e7a6
Ali-Asif1 <aliasif1485@yahoo.com>
2024-11-10 23:26:47 +0500

    project done

---
+++ script.js
@@ -0,0 +1,27 @@
+var form = document.getElementById('form');
+var resumeDisplay = document.getElementById('resume');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    var fName = document.getElementById('fname').value;
+    var contact = document.getElementById('phone').value;
+    var email = document.getElementById('email').value;
+    var dob = document.getElementById('dob').value;
+    var address = document.getElementById('address').value;
+    var education = document.getElementById('education').value;
+    var experience = document.getElementById('experience').value;
+    var skills = document.getElementById('skills').value;
+    var resumeFields = "\n    \n    <center><h1><b>Resume</b></h1></center>\n    <h3>Personal Information</h3>\n    <p><b>Name:</b><span contenteditable=\"true\">".concat(fName, "</span></p>\n    <p><b>Contact:</b><span contenteditable=\"true\">").concat(contact, "</span></p>\n    <p><b>Email:</b><span contenteditable=\"true\">").concat(email, "</span></p>\n    <p><b>DOB:</b><span contenteditable=\"true\">").concat(dob, "</span></p>\n    <p><b>Address:</b><span contenteditable=\"true\">").concat(address, "</span></p>\n    \n    <h3>Education Information</h3>\n    <p contenteditable=\"true\">").concat(education, "</p>\n    \n    <h3>Professional Information</h3>\n    <p contenteditable=\"true\">").concat(experience, "</p>\n    \n    <h3>Skills</h3>\n    <p contenteditable=\"true\">").concat(skills, "</p>\n    ");
+    if (resumeDisplay) {
+        resumeDisplay.innerHTML = resumeFields;
+    }
+    else {
+        console.error('some fields are missing');
+    }
+    // resume display on generate time
+    var display = resumeDisplay.style.display = 'block';
+    // working on pdf
+    var pdf = document.getElementById('btn-pdf');
+    pdf.addEventListener('click', function () {
+        window.print();
+    });
+});
---
+++ script.ts
@@ -0,0 +1,56 @@
+let form = document.getElementById('form') as HTMLFormElement
+let resumeDisplay = document.getElementById('resume') as HTMLDivElement
+
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    
+    let fName = (document.getElementById('fname') as HTMLInputElement).value
+    let contact = (document.getElementById('phone') as HTMLInputElement).value
+    let email = (document.getElementById('email') as HTMLInputElement).value
+    let dob = (document.getElementById('dob') as HTMLInputElement).value
+    let address = (document.getElementById('address') as HTMLTextAreaElement).value
+    let education = (document.getElementById('education') as HTMLTextAreaElement).value
+    let experience = (document.getElementById('experience') as HTMLTextAreaElement).value
+    let skills = (document.getElementById('skills') as HTMLTextAreaElement).value
+    
+    let resumeFields = `
+    
+    <center><h1><b>Resume</b></h1></center>
+    <h3>Personal Information</h3>
+    <p><b>Name:</b><span contenteditable="true">${fName}</span></p>
+    <p><b>Contact:</b><span contenteditable="true">${contact}</span></p>
+    <p><b>Email:</b><span contenteditable="true">${email}</span></p>
+    <p><b>DOB:</b><span contenteditable="true">${dob}</span></p>
+    <p><b>Address:</b><span contenteditable="true">${address}</span></p>
+    
+    <h3>Education Information</h3>
+    <p contenteditable="true">${education}</p>
+    
+    <h3>Professional Information</h3>
+    <p contenteditable="true">${experience}</p>
+    
+    <h3>Skills</h3>
+    <p contenteditable="true">${skills}</p>
+    `;
+    
+    if (resumeDisplay) {
+        resumeDisplay.innerHTML = resumeFields
+    } else {
+        console.error('some fields are missing');
+    }
+    
+    // resume display on generate time
+    
+    let display = resumeDisplay.style.display= 'block';
+    
+    
+    // working on pdf
+
+    let pdf= document.getElementById('btn-pdf') as HTMLButtonElement
+    pdf.addEventListener('click', function(){
+        window.print();
+    })
+    
+    
+})
